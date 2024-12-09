b8533129b505478add057849a57baa33b9ae8abc
XCOD33 <denistraju155@gmail.com>
2024-12-10 01:03:36 +0700

    ✨ (database): menambahkan konfigurasi database dan migrasi tabel baru 🔧 (docker-compose): memperbarui pengaturan docker-compose untuk MySQL
    Mengubah pengaturan koneksi database di file `.env` untuk menggunakan MySQL.
    Menambahkan file `docker-compose.db.yaml` untuk mengatur layanan database MySQL.
    Memperbarui file `docker-compose.yml` untuk mengintegrasikan layanan database dan menyesuaikan pengaturan restart.
    Menambahkan migrasi untuk tabel baru seperti `customers`, `orders`, `products`, dan lainnya untuk mendukung struktur database yang lebih baik.
    Perubahan ini dilakukan untuk meningkatkan manajemen data dan memudahkan pengembangan aplikasi.

--- lib/database/migrations/migrate.dart
+++ lib/database/migrations/migrate.dart
@@ -1,6 +1,11 @@
 import 'dart:io';
 import 'package:vania/vania.dart';
-import 'create_users_table.dart';
+import 'create_orderitems_table.dart';
+import 'create_customers_table.dart';
+import 'create_orders_table.dart';
+import 'create_vendors_table.dart';
+import 'create_products_table.dart';
+import 'create_productnotes_table.dart';
 
 void main(List<String> args) async {
   await MigrationConnection().setup();
@@ -15,10 +20,22 @@ void main(List<String> args) async {
 
 class Migrate {
   registry() async {
-    await CreateUserTable().up();
+    await CreateCustomersTable().up();
+    await CreateVendorsTable().up();
+    await CreateProductsTable().up();
+    await CreateOrdersTable().up();
+    await CreateOrderitemsTable().up();
+    await CreateProductnotesTable().up();
+    // await CreateUserTable().up();
   }
 
   dropTables() async {
-    await CreateUserTable().down();
+    // await CreateUserTable().down();
+    await CreateProductnotesTable().down();
+    await CreateOrderitemsTable().down();
+    await CreateOrdersTable().down();
+    await CreateProductsTable().down();
+    await CreateVendorsTable().down();
+    await CreateCustomersTable().down();
   }
 }
