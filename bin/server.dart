d1a83750887eba6b98d596f7e39c12fe9e08ae55
XCOD33 <denistraju155@gmail.com>
2024-12-09 22:41:52 +0700

    init

---
+++ bin/server.dart
@@ -0,0 +1,6 @@
+import 'package:vania/vania.dart';
+import 'package:paml_tugas_api/config/app.dart';
+
+void main() async {
+  Application().initialize(config: config);
+}
---
+++ lib/config/auth.dart
@@ -0,0 +1,9 @@
+import 'package:paml_tugas_api/app/models/user.dart';
+
+Map<String, dynamic> authConfig = {
+  'guards': {
+    'default': {
+      'provider': User(),
+    }
+  }
+};
