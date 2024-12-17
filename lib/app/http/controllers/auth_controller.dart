8d7e5ea2f69d49b0053b9aab252211f805fe3146
XCOD33 <denistraju155@gmail.com>
2024-12-17 13:18:29 +0700

    ✨ (auth_controller.dart): menambahkan fitur registrasi pengguna baru ♻️ (user_controller.dart): menghapus UserController yang tidak terpakai 🔧 (api_route.dart): memperbarui rute API untuk menggunakan AuthController
    Menambahkan fitur registrasi pengguna baru untuk meningkatkan fungsionalitas aplikasi. Pengguna dapat mendaftar dengan nama, email, dan password, serta mendapatkan umpan balik jika email sudah ada. Menghapus UserController yang tidak terpakai untuk menjaga kebersihan kode. Memperbarui rute API untuk menggunakan AuthController, sehingga semua fungsi otentikasi terpusat dan lebih mudah dikelola.

--- lib/app/http/controllers/auth_controller.dart
+++ lib/app/http/controllers/auth_controller.dart
@@ -2,6 +2,51 @@ import 'package:paml_tugas_api/app/models/user.dart';
 import 'package:vania/vania.dart';
 
 class AuthController extends Controller {
+  Future<Response> register(Request request) async {
+    try {
+      var name = request.input('name');
+      var email = request.input('email');
+      var password = request.input('password');
+
+      if (name == null || email == null || password == null) {
+        return Response.json({
+          'success': false,
+          'message': 'Name, email, and password are required',
+        });
+      }
+
+      var isEmailExist =
+          await User().query().where('email', '=', email).first();
+      if (isEmailExist != null) {
+        return Response.json({
+          'success': false,
+          'message': 'Email already exist',
+        });
+      }
+
+      final passwordHashed = Hash().make(password);
+      var user = await User().query().create({
+        'name': name,
+        'email': email,
+        'password': passwordHashed,
+        'created_at': DateTime.now().toIso8601String(),
+        'updated_at': DateTime.now().toIso8601String(),
+      });
+
+      return Response.json({
+        'success': true,
+        'message': 'User created successfully',
+        'data': user
+      });
+    } catch (e) {
+      return Response.json({
+        'success': false,
+        'message': 'Failed to create user',
+        'error': e.toString()
+      });
+    }
+  }
+
   Future<Response> login(Request request) async {
     try {
       final email = request.input('email');
--- lib/route/api_route.dart
+++ lib/route/api_route.dart
@@ -3,8 +3,8 @@ import 'package:paml_tugas_api/app/http/controllers/customer_controller.dart';
 import 'package:paml_tugas_api/app/http/controllers/order_controller.dart';
 import 'package:paml_tugas_api/app/http/controllers/product_controller.dart';
 import 'package:paml_tugas_api/app/http/controllers/product_note_controller.dart';
-import 'package:paml_tugas_api/app/http/controllers/user_controller.dart';
 import 'package:paml_tugas_api/app/http/controllers/vendor_controller.dart';
+import 'package:paml_tugas_api/app/http/middleware/authenticate.dart';
 import 'package:vania/vania.dart';
 import 'package:paml_tugas_api/app/http/controllers/home_controller.dart';
 import 'package:paml_tugas_api/app/http/middleware/home_middleware.dart';
@@ -28,48 +28,51 @@ class ApiRoute implements Route {
         .middleware([ErrorResponseMiddleware()]);
 
     Router.group(() {
-      Router.post('/', userController.store);
+      Router.post('/register', authController.register);
       Router.post('/login', authController.login);
-      Router.post('/logout', authController.logout);
-    }, prefix: '/users');
+      Router.post('/logout', authController.logout)
+          .middleware([AuthenticateMiddleware()]);
+    }, prefix: '/auth');
 
     Router.group(() {
-      Router.get('/', vendorController.index);
-      Router.post('/', vendorController.store);
-      Router.get('/{id}', vendorController.show);
-      Router.put('/{id}', vendorController.update);
-      Router.delete('/{id}', vendorController.destroy);
-    }, prefix: '/vendors');
+      Router.group(() {
+        Router.get('/', vendorController.index);
+        Router.post('/', vendorController.store);
+        Router.get('/{id}', vendorController.show);
+        Router.put('/{id}', vendorController.update);
+        Router.delete('/{id}', vendorController.destroy);
+      }, prefix: '/vendors');
 
-    Router.group(() {
-      Router.get('/', customerController.index);
-      Router.post('/', customerController.store);
-      Router.get('/{id}', customerController.show);
-      Router.put('/{id}', customerController.update);
-      Router.delete('/{id}', customerController.destroy);
-    }, prefix: '/customers');
+      Router.group(() {
+        Router.get('/', customerController.index);
+        Router.post('/', customerController.store);
+        Router.get('/{id}', customerController.show);
+        Router.put('/{id}', customerController.update);
+        Router.delete('/{id}', customerController.destroy);
+      }, prefix: '/customers');
 
-    Router.group(() {
-      Router.get('/', productController.index);
-      Router.post('/', productController.store);
-      Router.get('/{id}', productController.show);
-      Router.put('/{id}', productController.update);
-      Router.delete('/{id}', productController.destroy);
-    }, prefix: '/products');
+      Router.group(() {
+        Router.get('/', productController.index);
+        Router.post('/', productController.store);
+        Router.get('/{id}', productController.show);
+        Router.put('/{id}', productController.update);
+        Router.delete('/{id}', productController.destroy);
+      }, prefix: '/products');
 
-    Router.group(() {
-      Router.get('/', productNoteController.index);
-      Router.post('/', productNoteController.store);
-      Router.get('/{id}', productNoteController.show);
-      Router.put('/{id}', productNoteController.update);
-      Router.delete('/{id}', productNoteController.destroy);
-    }, prefix: '/product-notes');
+      Router.group(() {
+        Router.get('/', productNoteController.index);
+        Router.post('/', productNoteController.store);
+        Router.get('/{id}', productNoteController.show);
+        Router.put('/{id}', productNoteController.update);
+        Router.delete('/{id}', productNoteController.destroy);
+      }, prefix: '/product-notes');
 
-    Router.group(() {
-      Router.get('/', orderController.index);
-      Router.post('/', orderController.store);
-      Router.get('/{id}', orderController.show);
-      Router.delete('/{id}', orderController.destroy);
-    }, prefix: '/orders');
+      Router.group(() {
+        Router.get('/', orderController.index);
+        Router.post('/', orderController.store);
+        Router.get('/{id}', orderController.show);
+        Router.delete('/{id}', orderController.destroy);
+      }, prefix: '/orders');
+    }, middleware: [AuthenticateMiddleware()]);
   }
 }
